23fc657e17a40fbfe3427db893ea511ac1b7a4b5
maddiehutch1 <madhutch@byu.edu>
2024-10-26 13:41:25 -0600

    Initial Commit

---
+++ LearnTheLingoApp/LearnTheLingoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnTheLingoAppApp.swift
+//  LearnTheLingoApp
+//
+//  Created by IS 543 on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnTheLingoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
